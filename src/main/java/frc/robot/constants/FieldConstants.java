d0ab93da733968c0a94e0534bdbe4f2840709053
Noam Rosenberg <95913534+NoamRosenberg08@users.noreply.github.com>
2024-07-29 15:47:31 +0300

    Initialize photon-vision

---
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -0,0 +1,14 @@
+package frc.robot.constants;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.wpilibj.DriverStation;
+import org.photonvision.PhotonPoseEstimator;
+
+public class FieldConstants {
+
+    public static final DriverStation.Alliance RELATIVE_FIELD_CONVENTION_ALLIANCE = DriverStation.Alliance.Blue;
+
+    public static final double LENGTH_METERS = 16.54175;
+
+    public static final double WIDTH_METERS = 8.0137;
+}
