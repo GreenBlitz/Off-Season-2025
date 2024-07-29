9655492290c8bca14813c743a92d3bee32260778
GreenBlitz <greenblitz4590@gmail.com>
2024-07-29 15:58:37 +0300

    noam - added IIndexer

---
+++ src/main/java/frc/robot/subsystems/Indexer/IIndexer.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.Indexer;
+
+public interface IIndexer {
+
+    void setPower(double power);
+
+    void setVoltage(double voltage);
+
+    void setVelocity(double Velocity);
+
+    void setBrake();
+
+    void setCoast();
+
+}
