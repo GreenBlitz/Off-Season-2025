6f9327ee00f621f33738e4bae00ab4dc6095e998
Noam Rosenberg <95913534+NoamRosenberg08@users.noreply.github.com>
2024-07-29 15:47:26 +0300

    Initialize feature/turret-fix-inputs

---
+++ src/main/java/frc/robot/turret/TurretInputs.java
@@ -0,0 +1,14 @@
+package frc.robot.turret;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog;
+
+@AutoLog
+public class TurretInputs {
+
+    public Rotation2d position;
+    public Rotation2d velocity;
+    public double outputCurrent;
+    public double outputVoltage;
+
+}
---
+++ src/main/java/frc/robot/turret/TurretSubsystem.java
@@ -0,0 +1,87 @@
+package frc.robot.turret;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.utils.GBSubsystem;
+import org.littletonrobotics.junction.Logger;
+
+import java.util.Objects;
+
+import static frc.robot.turret.TurretConstants.LOGGING_PATH;
+
+public class TurretSubsystem extends GBSubsystem {
+
+    ITurret turret;
+    TurretState state;
+    TurretInputsAutoLogged inputs;
+
+    public TurretSubsystem(ITurret turret) {
+        this.turret = turret;
+        inputs = new TurretInputsAutoLogged();
+        turret.updateInputs(inputs);
+
+        this.state = TurretState.REST;
+    }
+
+
+    public void setState(TurretState targetState) {
+        this.state = targetState;
+    }
+
+    public Rotation2d getPosition() {
+        return inputs.position;
+    }
+
+    public Rotation2d getVelocity() {
+        return inputs.velocity;
+    }
+
+
+    public void handleRotateToPoint(Translation2d targetPoint, Translation2d robotPosition) {
+        Translation2d normalizedRobotPosition = targetPoint.minus(robotPosition);
+        this.turret.setPosition(
+                new Rotation2d(
+                        targetPoint.minus(robotPosition).getX(),
+                        targetPoint.minus(robotPosition).getY()
+                )
+        );
+    }
+
+    public void handleHoldPositionRelativeToRobot(Rotation2d targetAngle) {
+        this.turret.setPosition(Objects.requireNonNullElseGet(targetAngle, () -> Rotation2d.fromDegrees(0)));
+    }
+
+    public void handleHoldPositionRelativeToRobot() {
+       handleHoldPositionRelativeToRobot(inputs.position);
+    }
+
+    public void handleRest() {
+        this.turret.stop();
+    }
+
+    private void handleState(TurretState state) {
+        switch (state) {
+            case ROTATE_TO_POINT -> handleRotateToPoint(
+                    new Translation2d(),
+                    new Translation2d() /*waiting for swerve to do get from pose estimation*/
+            );
+            case HOLD_POSITION_RELATIVE_TO_ROBOT -> handleHoldPositionRelativeToRobot();
+            case REST -> handleRest();
+        }
+    }
+
+
+    @Override
+    protected String getLogPath() {
+        return LOGGING_PATH;
+    }
+
+    @Override
+    protected void subsystemPeriodic() {
+        handleState(this.state);
+
+        turret.updateInputs(inputs);
+        Logger.processInputs("Turret",inputs);
+    }
+
+}
