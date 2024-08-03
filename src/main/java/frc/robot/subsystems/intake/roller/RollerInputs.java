eb89d99a59467fbe3ad0bbdca5332299a97c46a2
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-08-03 23:50:51 +0300

    created inputs

---
+++ src/main/java/frc/robot/subsystems/intake/roller/RollerInputs.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.intake.roller;
+
+import org.littletonrobotics.junction.AutoLog;
+
+@AutoLog
+public class RollerInputs {
+
+    public double velocity;
+    public double appliedOutput;
+    public double outputCurrent;
+    public boolean isNoteIn;
+
+
+
+}
